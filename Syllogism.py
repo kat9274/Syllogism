6885897d0fd2ad2f8a559bf19e6f9f3a5ec43941
kat9274 <33795714+kat9274@users.noreply.github.com>
2017-11-21 15:18:12 -0600

    Add files via upload

---
+++ Syllogism.py
@@ -0,0 +1,12 @@
+import time
+s1 = raw_input("S1 >>> ")
+s2 = raw_input("S2 >>> ")
+As = raw_input("Subjects in S1 >>> ")
+av = raw_input("Verbs in S2 >>> ")
+ap = raw_input("Predicates in S2 >>> ")
+s = []
+s.append(As)
+s.append(av)
+s.append(ap)
+print(s)
+time.sleep(5)
