ad67a539b6beff176caeb07a30e8aea09f7a0b28
kat9274 <33795714+kat9274@users.noreply.github.com>
2017-12-03 18:27:54 -0600

    Update and rename Syllogism.py to Syllogism-Version_0.1.py

---
+++ Syllogism-Version_0.1.py
@@ -0,0 +1,7 @@
+s1 = raw_input("S1 >>> ")
+s2 = raw_input("S2 >>> ")
+As = raw_input("Subjects in S1 >>> ")
+Av = raw_input("Verbs in S2 >>> ")
+Ap = raw_input("Predicates in S2 >>> ")
+s = [As, Av, Ap]
+print(s)
